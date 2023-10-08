8fb981c77c796584d613c0270fc883fca220cc7b
doanhnh <17020637@vnu.edu.vn>
2023-10-08 14:18:24 +0700

    update deploymnet

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a4aa80c32a3164343b73bae6c135095a-793796855.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
